ebf203f38199fc3d56b655cd5cea10c0119ea20f
Albert Bertram <bertrama@umich.edu>
2018-02-21 20:10:10 -0500

    Make it configurable.

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 Slack does not seem to offer a possibility to forward channel posts via email. What Slack does offer is a Webhook. It can be configured to call a certain URL every time someone is posting into a certain channel or posting a certain trigger word.
 
-Take this simple PHP script, edit the configutaion section to your needs, put it on a suitable host and let a Webhook from you Slack Team call it. There!
+```ini
+; config.ini:
+to = "Example Name" <example@example.com>
+slack_token = XXXXXXXXXX
+domain = example.com
+```
--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 // Configuration
 // ============================================================
 // Take this token from slack's webhook integration settings
-$slack_token = "<paste-the-token-here>";
+$config = parse_ini_file('config.ini');
+
+$slack_token = $config['slack_token'];
+
+// Recipient of the emails
+$to = $config['to'];
 
 // The slack user who wrote the post (comes from Slack)
 $slack_user = $_POST["user_name"];
@@ -11,17 +16,14 @@ $slack_user = $_POST["user_name"];
 // The text of the posted message (comes from Slack)
 $slack_msg = $_POST["text"];
 
-// Recipient of the emails
-$to = "Frank Sinatra <frank@coolguy.org>";
-
 // From-header in the emails
-$from = $slack_user." via Slack <slack@gyourdomain.com>";
+$from = "{$slack_user}@{$config['domain']}";
 
 // Reply-to address in the emails
 $reply_to = $from;
 
 // Subject of the emails
-$subject = "@".$slack_user." wrote";
+$subject = "@{$slack_user} wrote";
 
 // ============================================================
 
@@ -29,17 +31,12 @@ $subject = "@".$slack_user." wrote";
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
 $headers[] = "Content-type: text/plain; charset=utf8";
-$headers[] = "From: ".$from;
-$headers[] = "Reply-To: ".$reply_to;
-$headers[] = "X-Mailer: PHP/".phpversion();
+$headers[] = "From: " . $from;
+$headers[] = "Reply-To: " . $reply_to;
+$headers[] = "X-Mailer: PHP/" . phpversion();
 
 // Send!
-if ($_POST["token"] == $slack_token)
-{
-	if (!mail($to, $subject, $slack_msg, implode("\r\n", $headers)))
-	{
-		echo "failed to send!";
-	}
+if ($_POST["token"] == $slack_token &&
+    !mail($to, $subject, $slack_msg, implode("\r\n", $headers))) {
+  echo "failed to send!";
 }
-
-?>
